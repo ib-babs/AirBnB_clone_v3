0eaecdec81304d5c400c41cf9186f57fa89148a1
ib-babs <babatundeibrahim426@gmail.com>
2024-02-26 18:31:49 +0100

    New updtae

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,20 +11,28 @@ from models.place import Place
 from models.review import Review
 
 
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
+
+
 @app_views.route('/status')
 def status():
     """Status of the api"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
-def count():
-    """Length of the api object"""
-    return jsonify({
-        'amenities': storage.count(Amenity),
-        'states': storage.count(State),
-        'reviews': storage.count(Review),
-        'users': storage.count(User),
-        'cities': storage.count(City),
-        'places': storage.count(Place)
-    })
+@app_views.route("/stats")
+def stats():
+    """Retrieve the number of each objects by type"""
+    statistics = {}
+
+    for key, value in classes.items():
+        statistics[key] = storage.count(value)
+
+    return jsonify(statistics)
